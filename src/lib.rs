0d902014d882af0c6550eef6b2a455cd524a220d
Degra02 <filippodegrandi02@gmail.com>
2024-06-18 11:59:35 +0200

    Add parse_all function

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,11 @@ pub mod objects;
 #[cfg(test)]
 mod tests;
 
+pub fn parse_all_cves() -> Vec<objects::cve_item::CveItem> {
+    let nvd_file = std::env::var("NVD_FILE").unwrap_or("data/nvdcve.json".to_string());
+    let nvd_str = std::fs::read_to_string(nvd_file).unwrap();
+    let nvd: objects::nvd::Nvd = serde_json::from_str(&nvd_str).unwrap();
+
+    nvd.cve_items
+}
+
