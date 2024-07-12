e2bebdbb4d6fb8a91e278da50fe03204e1bd196e
Degra02 <filippodegrandi02@gmail.com>
2024-07-12 11:41:28 +0200

    add baseMetricV2

--- src/objects/impact.rs
+++ src/objects/impact.rs
@@ -6,6 +6,31 @@ use super::utils::CvssV3Severity;
 pub struct Impact {
     #[serde(rename = "baseMetricV3", skip_serializing_if = "Option::is_none")]
     base_metric_v3: Option<BaseMetricV3>,
+    base_metric_v2: Option<BaseMetricV2>,
+
+    #[serde(rename = "impactScore", skip_serializing_if = "Option::is_none")]
+    impact_score: Option<f32>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    severity: Option<String>,
+
+    #[serde(rename = "exploitabilityScore", skip_serializing_if = "Option::is_none")]
+    exploitability_score: Option<f32>,
+
+    #[serde(rename = "obtainAllPrivilege", skip_serializing_if = "Option::is_none")]
+    obtain_all_privilege: Option<bool>,
+
+    #[serde(rename = "obtainOtherPrivilege", skip_serializing_if = "Option::is_none")]
+    obtain_other_privilege: Option<bool>,
+
+    #[serde(rename = "obtainUserPrivilege", skip_serializing_if = "Option::is_none")]
+    obtain_user_privilege: Option<bool>,
+
+    #[serde(rename = "userInteractionRequired", skip_serializing_if = "Option::is_none")]
+    user_interaction_required: Option<bool>,
+
+    #[serde(rename = "acInsufInfo", skip_serializing_if = "Option::is_none")]
+    ac_insuf_info: Option<bool>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -50,4 +75,39 @@ pub struct CvssV3 {
 
     #[serde(rename = "baseSeverity")]
     base_severity: CvssV3Severity,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BaseMetricV2 {
+    #[serde(rename = "cvssV2")]
+    cvss_v2: CvssV2,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CvssV2 {
+    version: String,
+
+    #[serde(rename = "vectorString")]
+    vector_string: String,
+
+    #[serde(rename = "accessVector")]
+    access_vector: String,
+
+    #[serde(rename = "accessComplexity")]
+    access_complexity: String,
+
+    #[serde(rename = "authentication")]
+    authentication: String,
+
+    #[serde(rename = "confidentialityImpact")]
+    confidentiality_impact: String,
+
+    #[serde(rename = "integrityImpact")]
+    integrity_impact: String,
+
+    #[serde(rename = "availabilityImpact")]
+    availability_impact: String,
+
+    #[serde(rename = "baseScore")]
+    base_score: f32,
 }
